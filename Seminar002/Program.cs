d78c03aa4377608d468e2118563f45dc265e99c9
Igor Yampolskiy <clubber_77@mail.ru>
2022-11-11 20:51:34 +0300

    added Seminar002

---
+++ Seminar002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberВ = Convert.ToInt32(Console.ReadLine());
+
+if(numberA*numberA == numberВ)
+    {
+        Console.WriteLine("Квадрат первого числа равен второму");
+    }
+else
+    {
+    Console.WriteLine("Квадрат первого числа не равен втрому");
+    }

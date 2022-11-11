aa2d0bfe90a448102359addf53212fbce61d43a7
Igor Yampolskiy <clubber_77@mail.ru>
2022-11-11 21:26:41 +0300

    little fix

--- Seminar003/Program.cs
+++ Seminar003/Program.cs
@@ -1,11 +1,14 @@
-﻿int mon = 1, tu = 2, wen = 3, th = 4, fri = 5, sun = 6, sat = 7;
-Console.Write("Введите номер дня недели:");
+﻿Console.Write("Введите номер дня недели:");
 int today = Convert.ToInt32(Console.ReadLine());
 
-if(today==mon) Console.WriteLine("Сегдня понедельник");
-if(today==tu) Console.WriteLine("Сегдня вторник");
-if(today==wen) Console.WriteLine("Сегдня среда");
-if(today==th) Console.WriteLine("Сегдня четверг");
-if(today==fri) Console.WriteLine("Сегдня пятница");
-if(today==sun) Console.WriteLine("Сегдня суббора");
-if(today==sat) Console.WriteLine("Сегдня воскресение");
+if(today<1) Console.WriteLine("Номер дня недели не может быть равен нулю");
+if(today>7) Console.WriteLine("Дней в неделе всего семь");
+if(today==1) Console.WriteLine("Сегодня понедельник");
+
+
+if(today==2) Console.WriteLine("Сегодня вторник");
+if(today==3) Console.WriteLine("Сегодня среда");
+if(today==4) Console.WriteLine("Сегодня четверг");
+if(today==5) Console.WriteLine("Сегодня пятница");
+if(today==6) Console.WriteLine("Сегодня суббора");
+if(today==7) Console.WriteLine("Сегодня воскресение");

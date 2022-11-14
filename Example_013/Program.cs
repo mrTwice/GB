d6fe231eb02f60feadf2d7ce00b618eabe11966b
Igor Yampolskiy <clubber_77@mail.ru>
2022-11-14 23:16:10 +0300

    lection_2

---
+++ Example_013/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {1, 2, 3, 4, 5, 6, 7, 18, 9};
+
+int n = array.Length;
+int find = 18;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}

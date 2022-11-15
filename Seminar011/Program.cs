3af3ea5c73c66e9b3819546be95be9b6b4b0124e
Igor Yampolskiy <clubber_77@mail.ru>
2022-11-15 21:37:52 +0300

    second seminar

---
+++ Seminar011/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = new Random().Next(10, 100);
+Console.WriteLine($"Cлучайное чило: {number}");
+int digit1 = number/10;
+int digit2 = number%10;
+
+if (digit1 > digit2)
+{
+    Console.WriteLine($"Наибольшая цифра в числе {number} это {digit1}");
+}
+else
+{
+        Console.WriteLine($"Наибольшая цифра в числе {number} это {digit2}");
+}
---
+++ Seminar012/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int N = new Random().Next(100, 1000);
+
+
+int firstNumber = N/100;
+int secondNumber = N%10;
+
+Console.Write($"{N} - >");
+Console.WriteLine($"{firstNumber}{secondNumber}");

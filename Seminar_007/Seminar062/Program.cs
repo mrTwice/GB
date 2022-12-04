9ff0e2ce2f1dddad65cafca9a4816a89f9f64520
Igor Yampolskiy <clubber_77@mail.ru>
2022-12-04 12:42:53 +0300

    Seminar_007

---
+++ Seminar_007/Seminar062/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, 
+// у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+
+
+Console.Clear();
+Console.WriteLine("Введите размерность двемерного массива m x n");
+Console.WriteLine("Введите m");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int[m,n];
+
+for (int i = 0; i < m; i++)
+    {
+        for (int j =0; j < n; j++)
+            {
+                matrix[i,j] = new Random().Next(0,11);
+                Console.Write(matrix[i,j] + " ");
+            }
+        Console.WriteLine();
+    }
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+
+for (int i = 0; i < m; i++)
+    {
+        for (int j =0; j < n; j++)
+            {
+                if (i%2 == 0 & j%2 == 0)
+                    {
+                        matrix[i,j] = matrix[i,j] * matrix[i,j];
+                    }
+                Console.Write(matrix[i,j] + " ");
+            }
+        Console.WriteLine();
+    }
---
+++ Seminar_007/Seminar064/Program.cs
@@ -0,0 +1,40 @@
+﻿
+// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+Console.Clear();
+Console.WriteLine("Введите размерность двемерного массива m x n");
+Console.WriteLine("Введите m");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int[m,n];
+
+for (int i = 0; i < m; i++)
+    {
+        for (int j =0; j < n; j++)
+            {
+                matrix[i,j] = new Random().Next(0,11);
+                Console.Write(matrix[i,j] + " ");
+            }
+        Console.WriteLine();
+    }
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+int mainSum = 0;
+for (int i = 0; i < m; i++)
+    {
+        for (int j =0; j < n; j++)
+            {
+                if (i == j) mainSum = mainSum + matrix[i,j];
+            }
+    }
+Console.WriteLine(mainSum);
